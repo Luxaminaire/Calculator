777f4e67a2a8cbdbb3539e5c173e6eeadbaa42ec
jake <beej8046@gmail.com>
2021-09-23 18:13:25 -0800

    Calculator Progress

--- app.js
+++ app.js
@@ -1,37 +1,85 @@
-class Calc {
-    constructor() {
-        this.a = 0;
-        this.b = 0;
-        this.operator = "";
-    }
+let a = "";
+let b = "";
+let operator = "";
+let currentOperation = ["a", "b"];
+let o = 0;
+let operators = "+-*/";
+let digits= "0123456789";
+    
+   function engaged(e) {
+        let onClickTarget = e.target.innerText;
+        let output = document.getElementById('display');
+        if (digits.includes(onClickTarget) && currentOperation[o] === "a") {
+            a += onClickTarget;
+            output.innerText = a;
+        }
 
-    add(a, b) {
-        a + b;
-    }
+        if (digits.includes(onClickTarget) && currentOperation[o] === "b") {
+            b += onClickTarget;
+            output.innerText = b;
+        }
 
-    subtract(a, b) {
-        a - b;
-    }
+        if (a.length > 0 && operators.includes(onClickTarget) ) {
+            operator = onClickTarget;
+            o++;
+        }
 
-    multiply(a, b) {
-        a * b;
-    }
+        if (onClickTarget === "C") {
+            let output = document.getElementById('display');
+            o = 0;
+            a = "";
+            b = "";
+            operator = "";
+            output.innerText = "";
+        }
 
-    divide(a, b) {
-        if (b !== 0) {
-            return a / b;
+        if(onClickTarget === "=" && a.length > 0 && b.length > 0 ) {
+            let val = operate(a, b, operator);
+            output.innerText = val;
+            a = val;
+            o = 1;
         }
     }
 
-    operate(a, b, operator) {
+    function add(a, b) {
+        return Number(a) + Number(b);
+    }
+
+    function subtract(a, b) {
+        return Number(a) - (b);
+    }
+
+    function  multiply(a, b) {
+        return Number(a) * Number(b);
+    }
+
+    function divide(a, b) {
+        if (Number(b) !== 0) {
+            return Number(a) / Number(b);
+        }
+    }
+
+    function operate(a, b, operator) {
         if (operator === "+") {
-            add(a,b);
+           return add(a,b);
         } else if (operator === "-") {
-            subtract(a,b);
+            return subtract(a,b);
         } else if (operator === "*") {
-            multiply(a,b);
-        }  else {
-            divide(a,b);
+            return multiply(a,b);
+        }  else if (operator === "/"){
+            return divide(a,b);
         }
     }
+
+let output = document.getElementById('display');
+    output.innerText = "";
+
+function makeOnClicks(){
+    let eles = [...document.querySelectorAll('.cell')];
+    
+    eles.forEach(element => {
+        element.addEventListener('click', engaged);
+    });
 }
+
+makeOnClicks();
